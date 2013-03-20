9d83c6a8f478ed54a7326583f82b1eb4b0a04c62
Stu Slack <dev@eslacks.com>
2013-03-20 10:37:53 -0700

    Initial add of Matchismo

---
+++ Matchismo/GameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultsViewController.h
+//  Matchismo
+//
+//  Created by Stuart Slack on 3/13/13.
+//  Copyright (c) 2013 Redtail Software, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultsViewController : UIViewController
+
+@end
